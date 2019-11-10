ab837b42e2e6c5cfa192841efdeb09b667b6b2ca
rob-DEV <masseymann@outlook.com>
2019-11-10 01:27:54 +0000

    ImGUI tested begining abstraction

--- Onyx/src/Onyx/Core/Application.cpp
+++ Onyx/src/Onyx/Core/Application.cpp
@@ -11,6 +11,11 @@
 
 #include <GLFW/glfw3.h>
 
+#include "imgui.h"
+#include "imgui_internal.h"
+#include "examples/imgui_impl_glfw.h"
+#include "examples/imgui_impl_opengl3.h"
+
 namespace Onyx {
 	
 	Application* Application::s_Instance = nullptr;
@@ -37,6 +42,9 @@ namespace Onyx {
 		delete m_Window;
 	}
 
+	void MySaveFunction() {
+
+	}
 	void Application::run()
 	{
 		Renderer2D::init();
@@ -47,10 +55,61 @@ namespace Onyx {
 		double previousTime = glfwGetTime();
 		int frameCount = 0;
 
+		//imgui test
+		IMGUI_CHECKVERSION();
+		ImGui::CreateContext();
+		ImGuiIO& io = ImGui::GetIO(); (void)io;
+		io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;       // Enable Keyboard Controls
+		//io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      // Enable Gamepad Controls
+		io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;           // Enable Docking
+		io.ConfigFlags |= ImGuiConfigFlags_ViewportsEnable;         // Enable Multi-Viewport / Platform Windows
+		//io.ConfigFlags |= ImGuiConfigFlags_ViewportsNoTaskBarIcons;
+		//io.ConfigFlags |= ImGuiConfigFlags_ViewportsNoMerge;
+
+		// Setup Dear ImGui style
+		ImGui::StyleColorsDark();
+		//ImGui::StyleColorsClassic();
+
+		// When viewports are enabled we tweak WindowRounding/WindowBg so platform windows can look identical to regular ones.
+		ImGuiStyle& style = ImGui::GetStyle();
+		if (io.ConfigFlags & ImGuiConfigFlags_ViewportsEnable)
+		{
+			style.WindowRounding = 0.0f;
+			style.Colors[ImGuiCol_WindowBg].w = 1.0f;
+		}
+
+		Application& app = *Application::get();
+		GLFWwindow* window = static_cast<GLFWwindow*>(app.getOnyxWindow().getNativeWindow());
+
+		// Setup Platform/Renderer bindings
+		ImGui_ImplGlfw_InitForOpenGL(window, true);
+		ImGui_ImplOpenGL3_Init("#version 410");
+
+		// Setup Dear ImGui style
 		while (!m_Window->isClosed()) {
+			
 
 			double currentTime = glfwGetTime();
 			frameCount++;
+
+
+			ImGui_ImplOpenGL3_NewFrame();
+			ImGui_ImplGlfw_NewFrame();
+			ImGui::NewFrame();
+			static bool show = true;
+			ImGui::ShowDemoWindow(&show);
+		
+
+			ImGui::Render();
+			ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
+			if (io.ConfigFlags & ImGuiConfigFlags_ViewportsEnable)
+			{
+				GLFWwindow* backup_current_context = glfwGetCurrentContext();
+				ImGui::UpdatePlatformWindows();
+				ImGui::RenderPlatformWindowsDefault();
+				glfwMakeContextCurrent(backup_current_context);
+			}
 
 			if (currentTime - previousTime >= 1.0)
 			{
