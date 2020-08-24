e2e4b287000123cd6a481cfaf87b0f4b1ca2ee70
Robert <Robert@ROBERTPC>
2020-08-24 20:21:41 +0100

    Re-organised WPF XAML

---
+++ Onyx-Editor-NET/src/Onyx-Editor-NET/Debug/Console.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+using System.IO;
+
+namespace Onyx_Editor_NET.Debug
+{
+    public class Console
+    {
+        // P/Invoke required:
+        private const UInt32 StdOutputHandle = 0xFFFFFFF5;
+        [DllImport("kernel32.dll")]
+        private static extern IntPtr GetStdHandle(UInt32 nStdHandle);
+        [DllImport("kernel32.dll")]
+        private static extern void SetStdHandle(UInt32 nStdHandle, IntPtr handle);
+        [DllImport("kernel32")]
+        static extern bool AllocConsole();
+
+        public static void Create()
+        {
+            AllocConsole();
+
+        }
+
+    }
+}
